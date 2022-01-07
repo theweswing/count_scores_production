7cace75148361470dbad83c9c7b175150a07f80c
Yuyi <yuyi365@gmail.com>
2022-01-07 10:40:45 -0500

    migrated front and back end

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,8 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
+
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
